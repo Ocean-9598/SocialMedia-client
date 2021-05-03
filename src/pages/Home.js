1800e9a0d27c17c877d7a977cda70650622ce864
Ocean-9598 <barusagar007@gmail.com>
2021-05-03 13:34:51 -0500

    update getposts variable usage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,7 +38,7 @@ function Home() {
           ) : (
             <Transition.Group>
                 {
-                    posts && posts.map(post => (
+                    getposts && getposts.map(post => (
                         <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
                             <PostCard post={post} />
                       </Grid.Column> 
